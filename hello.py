74a05a09809fca67f86b4219a9bbf5e4ad2db96d
Jay Khade <jaykhade2020@gmail.com>
2023-11-16 18:11:34 +0530

    Flask-wtf - created form

--- hello.py
+++ hello.py
@@ -1,8 +1,88 @@
 from flask import Flask, render_template
 
+# to create a form object
+from flask_wtf import FlaskForm
+
+# string input box, submit button, imagefield for images
+from wtforms import StringField, SubmitField
+
+# if something is not filled or empty, then gives warning
+# different validators for email, password, etc
+from wtforms.validators import DataRequired
+
+
+# with form there is csrf token -- it will create a secrete key with form,
+# and behind the scene it will synch with another secrete key.
+# making sure that hacker won't hijack the form,
+# we need to create a secrete key for backend
+
 # create a flask instance
 # __name__ helps flask search for project files
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "This is a secrate key which not ment to be shared"
+
+
+# create a form class
+# inheriting with a flask form
+class NamerForm(FlaskForm):
+    name = StringField("Whats Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+# create name page
+@app.route("/name", methods=["GET", "POST"])
+def name():
+    name = None
+
+    # blank form
+    form = NamerForm()
+
+    # validate form
+    if form.validate_on_submit():
+        # if someone posted a name, it will be stored here
+        name = form.name.data
+
+        # it will clear the name-field for the next fill
+        form.name.data = None
+    return render_template("name.html", name=name, form=form)
+
+
+## Fields
+# BooleanField
+# DateField
+# DateTimeField
+# DecimalField
+# FileField
+# HiddenField
+# MultipleField
+# FieldList
+# FloatField
+# FormField
+# IntegerField
+# PasswordField
+# RadioField
+# SelectField
+# SelectMultipleField
+# SubmitField
+# StringField
+# TextAreaField
+
+## Validators
+# DataRequired
+# Email
+# EqualTo
+# InputRequired
+# IPAddress
+# Length
+# MacAddress
+# NumberRange
+# Optional
+# Regexp
+# URL
+# UUID
+# AnyOf
+# NoneOf
+
 
 # jinja2 Filters
 # safe - send html apply tags
